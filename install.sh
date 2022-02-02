1e14a79654b99a1cd5a791d88d664f31ad5207aa
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-02-02 20:16:49 +0900

    lab3になりtensorboardとappmodeが動かない、vimの問題は解決

--- install.sh
+++ install.sh
@@ -1,48 +1,21 @@
 #!/bin/bash
+set -euxo pipefail
+name=lab3
 
-# https://github.com/krassowski/jupyterlab-lsp#installation
-conda install -y -c conda-forge 'jupyterlab>=2,<2.1.0a0'
-pip install jupyter-lsp
-conda install -y -c conda-forge nodejs
-jupyter labextension install @krassowski/jupyterlab-lsp
-conda install -c conda-forge python-language-server
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+conda env remove -y -n "${name}"
+conda create -y -n "${name}"
+conda activate "${name}"
+conda install -y -c conda-forge jupyterlab=3 ipycanvas appmode jupyterlab-lsp 'nodejs>=12.0.0' python-lsp-server
 
-# https://github.com/chaoleili/jupyterlab_tensorboard#development
-pip install jupyter-tensorboard
-# jupyter labextension install jupyterlab_tensorboard # not for jupyterlab 2
-git clone https://github.com/chaoleili/jupyterlab_tensorboard
-cd jupyterlab_tensorboard
-    npm install
-    npm run build
-    jupyter labextension link .
-cd -; rm -rf jupyterlab_tensorboard
+# pip install tensorboard
+# pip install jupyter-tensorboard 
+# pip install git+https://github.com/cliffwoolley/jupyter_tensorboard.git
+# pip install git+https://github.com/chaoleili/jupyterlab_tensorboard.git  死ぬ
 
-# https://github.com/jwkvam/jupyterlab-vim/pull/115
-git clone https://github.com/axelfahy/jupyterlab-vim -b jlab2.0
-cd jupyterlab-vim
-    jlpm install
-    jlpm run build
-    jupyter labextension link .
-cd -; rm -rf jupyterlab-vim
-
-# ipycanvas
-jupyter labextension install @jupyter-widgets/jupyterlab-manager ipycanvas
-# https://anaconda.org/conda-forge/ipycanvas
-conda install -c conda-forge ipycanvas
-
-
-conda install -c conda-forge appmode
-
-# update all extension
-jupyter labextension update --all
-
-## new lab3
-exit
-conda install -y -c conda-forge jupyterlab=3 ipycanvas appmode jupyterlab-lsp
-pip install git+https://github.com/cliffwoolley/jupyter_tensorboard.git
-conda install -y -c conda-forge 'nodejs>=12.0.0' 
-pip install git+https://github.com/chaoleili/jupyterlab_tensorboard.git
+# Escが効きすぎる https://github.com/jupyterlab-contrib/jupyterlab-vim/issues/31#issue-837847379
 pip install jupyterlab_vim
+pip install jupyterlab-vimrc #  C-, -> vimrcに {"unnamedplus": true}
 pip install ipyfilechooser
 pip install jupyterlab_widgets
 jupyter labextension install jupyterlab-plotly
